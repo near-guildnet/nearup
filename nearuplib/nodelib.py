cc3f035aaa911ffe400678bfda40824819ff672d
near-guildnet <67438620+near-guildnet@users.noreply.github.com>
2021-03-31 11:25:01 -0500

    Update to 1.18.1-rc.2

--- nearuplib/nodelib.py
+++ nearuplib/nodelib.py
@@ -168,7 +168,7 @@ def net_to_branch(net):
     if net == 'testnet':
         return 'stable'
     elif net == 'guildnet':
-        return '1.17.0-rc.5'
+        return '1.18.1-rc.2'
     elif net == 'betanet':
         return 'beta'
     else:
